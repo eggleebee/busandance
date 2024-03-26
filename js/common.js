9a5cb0ea7552f5c3562e41d3ac436f52f7df25ac
PC-300\PC-300 <rani1791@naver.com>
2024-03-26 16:19:43 +0900

    첫 업로드

---
+++ js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    // gnb
+
+
+    // 스와이퍼
+    var swiper = new Swiper(".judge_swiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+
+})
